c5391bf0568301efd15e1d49e05df8f17ca2000d
Yashashvi Bhardwaj <134696197+frontend-C0de@users.noreply.github.com>
2023-06-08 12:17:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+
+export{
+    Navbar,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonial,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+};
